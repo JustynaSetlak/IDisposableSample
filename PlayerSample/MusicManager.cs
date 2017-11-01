92b430609e79d9eff25393edb8d2de4c39ca886b
jsetlak@pgs-soft.com <justa96.s1@gmail.com>
2017-11-01 19:35:15 +0100

    added sample with mp3

---
+++ PlayerSample/MusicManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace PlayerSample
+{
+    public class MusicManager : IDisposable
+    {
+        private readonly WaveOutEvent _waveOutEvent;
+        private readonly Mp3FileReader _mp3FileReader;
+        private bool _isDisposedWaveOutEvent;
+        private bool _isDisposedMp3FileReader;
+
+        public MusicManager(WaveOutEvent waveOutEvent, Mp3FileReader mp3FileReader)
+        {
+            _waveOutEvent = waveOutEvent;
+            _mp3FileReader = mp3FileReader;
+        }
+
+        public void PlayMusic()
+        {
+            Console.WriteLine("Playing music");
+            CheckIfDisposed();
+            _waveOutEvent.Init(_mp3FileReader);
+            _waveOutEvent.Play();
+        }
+
+        public void StopMusic()
+        {
+            Console.WriteLine("Stopping music");
+            _waveOutEvent.Stop();
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            DisposeWaveOutEvent();
+            DisposeMp3FileReader();
+        }
+
+        private void DisposeWaveOutEvent()
+        {
+            Console.WriteLine("Check if should dispose WaveOutEvent");
+            if (!_isDisposedWaveOutEvent)
+            {
+                Console.WriteLine("Disposing WaveOutEvent");
+                _waveOutEvent.Dispose();
+                _isDisposedWaveOutEvent = true;
+            }
+            else
+            {
+                Console.WriteLine("Already disposed WaveOutEvent");
+            }
+        }
+
+        private void DisposeMp3FileReader()
+        {
+            Console.WriteLine("Check if should dispose Mp3FileReader");
+            if (!_isDisposedMp3FileReader)
+            {
+                Console.WriteLine("Disposing Mp3FileReader");
+                _mp3FileReader.Dispose();
+                _isDisposedMp3FileReader = true;
+            }
+            else
+            {
+                Console.WriteLine("Already disposed Mp3FileReader");
+            }
+        }
+
+        private void CheckIfDisposed()
+        {
+            Console.WriteLine("Checking if any object disposed");
+            if (_isDisposedWaveOutEvent || _isDisposedMp3FileReader)
+            {
+                throw new ObjectDisposedException("Object disposed");
+            }
+        }
+    }
+}
---
+++ PlayerSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace PlayerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicManager musicManager = new MusicManager(new WaveOutEvent(), new Mp3FileReader("Mozart.mp3"));
+            musicManager.PlayMusic();
+            
+            Console.WriteLine("if you want to stop music write: stop");
+            var command = Console.ReadLine();
+            if (command == "stop")
+            {
+                musicManager.StopMusic();
+            }
+            Console.WriteLine("Write play to play music");
+            var playCommand = Console.ReadLine();
+            if (playCommand == "play")
+            {
+                musicManager.PlayMusic();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
